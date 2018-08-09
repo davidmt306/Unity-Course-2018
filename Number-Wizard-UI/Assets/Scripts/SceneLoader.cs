dc5a990af495f1e20aa72380e8f989fb43283826
davidmt306 <davidmt_306@hotmail.com>
2018-08-08 19:02:40 -0500

    Load all scenes

---
+++ Number-Wizard-UI/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+    public void LoadNextScene() {
+        // Variable que almacena el index de la escena actual
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        // Carga la escena siguiente, sumando 1 al index actual
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void LoadStartScene() {
+        SceneManager.LoadScene(0);
+        Debug.Log("YES");
+    }
+}
