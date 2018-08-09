a725e8f8545ba5ad86ff0a3fc9862fb2cb96b6f4
davidmt306 <davidmt_306@hotmail.com>
2018-08-09 01:28:21 -0500

    Colliders, collision & triggers

---
+++ Block-Breaker/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+    public void LoadNextScene() {
+        // Variable que almacena el index de la escena actual
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        // Carga la escena siguiente, sumando 1 al index actual
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+    // Carga la escena con el index en la posicion 0
+    public void LoadStartScene() {
+        SceneManager.LoadScene(0); 
+    }
+    // Salir del juego
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
