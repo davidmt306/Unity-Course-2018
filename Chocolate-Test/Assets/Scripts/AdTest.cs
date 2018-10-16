927e9ec09af87ffadb658f50b74dcb68358ec44b
davidmt306 <davidmt_306@hotmail.com>
2018-10-16 13:25:21 -0500

    Chocolate plugin added

---
+++ Chocolate-Test/Assets/Scripts/AdTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Vdopia;
+
+public class AdTest : MonoBehaviour {
+
+    VdopiaPlugin plugin;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void requestRewardAd() {
+        Debug.Log("Reward video requested");
+        if (Application.platform == RuntimePlatform.Android && plugin != null) {
+            plugin.RequestRewardAd("XqjhRR");
+        }
+    }
+}
