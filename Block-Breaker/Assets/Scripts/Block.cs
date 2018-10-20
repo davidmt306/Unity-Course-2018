f03e252ccef4630fdde0338b19c01aba424c84cf
davidmt306 <davidmt_306@hotmail.com>
2018-10-20 13:47:20 -0500

    Fraction too much friction

---
+++ Block-Breaker/Assets/Scripts/Block.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour {
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        Destroy(gameObject);
+        // Prints the object that collides with the block
+        //Debug.Log(collision.gameObject.name); 
+    }
+
+}
