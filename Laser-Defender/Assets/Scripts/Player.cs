81d4b71032dc08eecffa58e586f2af2e9a392176
davidmt306 <davidmt_306@hotmail.com>
2018-10-23 12:09:25 -0500

    VewportToWorldPoint();
    - Player movement

---
+++ Laser-Defender/Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    // Configuration parameters
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float xPadding = 0.8f;
+    [SerializeField] float yPadding = 1f;
+
+    float xMin;
+    float xMax;
+
+    float yMin;
+    float yMax;
+
+	// Use this for initialization
+	void Start () {
+        SetUpMoveBoundaries();
+	}
+
+    private void SetUpMoveBoundaries() {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + xPadding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - xPadding;
+
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + yPadding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - yPadding;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Move();
+	}
+
+    private void Move() {
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
+        transform.position = new Vector2(newXPos, newYPos);
+    }
+}
