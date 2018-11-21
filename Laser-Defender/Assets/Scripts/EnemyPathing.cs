7af3690a68d3177b5cc6c7a1ea698079a7b8fc4c
davidmt306 <davidmt_306@hotmail.com>
2018-11-21 13:47:54 -0600

    Using a Foreach loop

--- Laser-Defender/Assets/Scripts/EnemyPathing.cs
+++ Laser-Defender/Assets/Scripts/EnemyPathing.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class EnemyPathing : MonoBehaviour {
     // Configuration parameters
-    [SerializeField] List<Transform> waypoints;
+    [SerializeField] WaveConfig waveConfig;
+    List<Transform> waypoints;
+    [SerializeField] float moveSpeed = 2f;
+    int waypointIndex = 0;
 
 	// Use this for initialization
 	void Start () {
-		
+        waypoints = waveConfig.GetWaypoints();
+        transform.position = waypoints[waypointIndex].transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        Move();
+    }
+
+    private void Move() {
+        if (waypointIndex <= waypoints.Count - 1) {
+            var targetPosition = waypoints[waypointIndex].transform.position;
+            var movementThisFrame = moveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards
+                (transform.position, targetPosition, movementThisFrame);
+
+            if (transform.position == targetPosition) {
+                waypointIndex++;
+            }
+        } else {
+            Destroy(gameObject);
+        }
+    }
 }
