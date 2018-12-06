22bd96427e3234e8138e27375457175aeea6227f
davidmt306 <davidmt_306@hotmail.com>
2018-12-05 19:45:17 -0600

    WaveConfig For Path & Speed

--- Laser-Defender/Assets/Scripts/EnemyPathing.cs
+++ Laser-Defender/Assets/Scripts/EnemyPathing.cs
@@ -4,9 +4,8 @@ using UnityEngine;
 
 public class EnemyPathing : MonoBehaviour {
     // Configuration parameters
-    [SerializeField] WaveConfig waveConfig;
+    WaveConfig waveConfig;
     List<Transform> waypoints;
-    [SerializeField] float moveSpeed = 2f;
     int waypointIndex = 0;
 
 	// Use this for initialization
@@ -20,10 +19,14 @@ public class EnemyPathing : MonoBehaviour {
         Move();
     }
 
+    public void SetWaveConfig(WaveConfig waveConfig) {
+        this.waveConfig = waveConfig;
+    }
+
     private void Move() {
         if (waypointIndex <= waypoints.Count - 1) {
             var targetPosition = waypoints[waypointIndex].transform.position;
-            var movementThisFrame = moveSpeed * Time.deltaTime;
+            var movementThisFrame = waveConfig.GetMoveSpeed() * Time.deltaTime;
             transform.position = Vector2.MoveTowards
                 (transform.position, targetPosition, movementThisFrame);
 
