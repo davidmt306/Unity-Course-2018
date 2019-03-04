fc13244241345a53b60b6c6eefaba62c2e8c2ff7
davidmt306 <davidmt_306@hotmail.com>
2019-03-04 11:49:53 -0600

    Spawn at mouse position

---
+++ Glitch-Garden/Assets/Scripts/Health.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+    // Configuration parameters
+    [SerializeField] float health = 100f;
+
+    Animator animator;
+
+    private void Start() {
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    public void DealDamage(float damage) {
+        health -= damage;
+        if (health <= 0) {
+            animator.SetTrigger("Death"); 
+        }
+    }
+
+    public void DestroyZombie() {
+        Destroy(gameObject, 1f);
+    }
+}
---
+++ Glitch-Garden/Assets/Scripts/SoldierSpawner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoldierSpawner : MonoBehaviour {
+    //Configuration parameters
+    [SerializeField] GameObject soldier;
+
+    private void OnMouseDown() {
+        SpawnSoldier();
+    }
+
+    private void SpawnSoldier() {
+        GameObject newSoldier = Instantiate(soldier, transform.position, Quaternion.identity) as GameObject;
+    }
+}
