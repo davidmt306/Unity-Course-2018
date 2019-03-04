3685e4804f545b28fe325dfb4f58724301cb0ed1
davidmt306 <davidmt_306@hotmail.com>
2019-03-04 13:28:54 -0600

    Create Defender selection UI

---
+++ Glitch-Garden/Assets/Scripts/SoldierButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoldierButton : MonoBehaviour {
+
+    private void OnMouseDown() {
+        var buttons = FindObjectsOfType<SoldierButton>();
+        foreach(SoldierButton button in buttons) {
+            button.GetComponent<SpriteRenderer>().color = new Color32(41, 41, 41, 255); 
+        }
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+}
--- Glitch-Garden/Assets/Scripts/SoldierSpawner.cs
+++ Glitch-Garden/Assets/Scripts/SoldierSpawner.cs
@@ -7,10 +7,24 @@ public class SoldierSpawner : MonoBehaviour {
     [SerializeField] GameObject soldier;
 
     private void OnMouseDown() {
-        SpawnSoldier();
+        SpawnSoldier(GetSquareClicked());
     }
 
-    private void SpawnSoldier() {
-        GameObject newSoldier = Instantiate(soldier, transform.position, Quaternion.identity) as GameObject;
+    private Vector2 GetSquareClicked () {
+        Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
+        Vector2 gridPos = SnapToGrid(worldPos);
+        return gridPos;
+    }
+
+    private Vector2 SnapToGrid(Vector2 rawWorldPos) {
+        float newX = Mathf.RoundToInt(rawWorldPos.x);
+        float newY = Mathf.RoundToInt(rawWorldPos.y);
+        return new Vector2(newX, newY);
+    }
+
+    private void SpawnSoldier(Vector2 RoundedPos) {
+        GameObject newSoldier = Instantiate(soldier, RoundedPos, Quaternion.identity) as GameObject;
+        newSoldier.transform.Rotate(0, -180, 0);  
     }
 }
